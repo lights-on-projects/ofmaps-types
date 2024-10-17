85a52919a9271a2909eb53cd6cb29f158237f4ff
Artyom Ugryumov <artiom.ygryumov@gmail.com>
2024-10-17 16:17:10 +0300

    add: Добавить декларацию типов для project_metadata

---
+++ src/common.ts
@@ -0,0 +1,70 @@
+/**
+ * Названия и id плагинов
+ *
+ * @link https://demoadmin.officescheme.ru/admin/Info/API?action=list_plugins
+ */
+export enum Plugins {
+  /**
+   * Сохраняет инфу о том кем и когда была создана бронь
+   */
+  BookingAuthoring = 'pacd6f213c8bcb444aba21730cf063d3',
+
+  /**
+   * Спамит пользователей инфой о созданных бронях
+   */
+  BookingMailBomber = 'p6b4f6ae7af4c49009e23e4062ad6e977',
+
+  /**
+   * Расширенные правила бронирования
+   */
+  BookingRules = 'p9f599e30a6f14828970042bb50d61e9e',
+
+  /**
+   * Расширяет подсказку точки пользовательскими данными. Требуется полевой плагин
+   */
+  PointTooltip = 'aae17fff117f4a16a50fa1288c4ed9e8',
+
+  /**
+   * Отправляет сообщения в rsyslog
+   */
+  rfc5424 = 'ada143e2081194ba98eefb45fad6c2365',
+
+  /**
+   * Пользователи могут забронировать некоторые типы объектов на любое время
+   */
+  Bookings = '53d02367136147b8b5187d109256ce74',
+
+  /**
+   * Размещает новые точки в центре многоугольника, помещает перенесенные точки в центр многоугольника.
+   */
+  Centrator = 'e08f6b0a0dd34ef8ab2e405661d3b8ea',
+
+  /**
+   * Позволяет определять расширенные поля в метаданных и использовать их в слоях
+   */
+  ExtendedFields = 'f7222e6eb7114fbd865be059646cbc53',
+
+  /**
+   * Включает аутентификацию OpenID в воркспейсе
+   */
+  openid = 'pac2dc85217d4e4477c8fca8c2e27272f18',
+
+  /**
+   * Включает аутентификацию Saml в воркспейсе
+   */
+  saml = 'pac867b6def6c4d9fb71ffdb975051f0d',
+
+  /**
+   * Позволяет определять расширенные поля для пользователей и предоставляет некоторые утилиты для импорта пользователей
+   */
+  UserFields = 'd56e0e5438ea4f7e81c820fdd380ef4e',
+}
+
+export type AuthToken = string & { __brand: 'AuthToken' }
+export type WorkspaceID = number & { __brand: 'WorkspaceID' }
+export type ProjectID = number & { __brand: 'ProjectID' }
+export type LayerID = string & { __brand: 'LayerID' }
+export type PointID = string & { __brand: 'PointID' }
+export type HEX = string & { __brand: 'HEX' }
+
+export type UserRole = 'admin' | 'client' | 'none'
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export * from './api/project-metadata.types'
+export * from './common'
