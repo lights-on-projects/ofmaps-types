00c582b679632ba0d4cbb6e16f324e63992b4a1a
Artyom Ugryumov <artiom.ygryumov@gmail.com>
2024-11-05 12:50:42 +0300

    add: Добавить общий тип ProjectUserDataType

---
+++ src/shared/project-user-data.ts
@@ -0,0 +1,25 @@
+import { Plugins, UserRole } from '../common'
+
+/**
+ * Данные пользователя для работы с проектом
+ */
+export type ProjectUserDataType = {
+  /**
+   * Права доступа к текущему проекту
+   *
+   * То же самое, что и `project_access`
+   */
+  role: UserRole
+
+  /**
+   * Права доступа к воркспейсу
+   */
+  wsa: UserRole
+
+  /**
+   * Настройки плагина для текущего пользователя
+   *
+   * Ключи объекта это ID плагинов
+   */
+  plugin_data: Partial<Record<`${Plugins}`, Record<string, any>>>
+}
