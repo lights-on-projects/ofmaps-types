f6d8ea44a04f766b3f48c6fd71eb51cce66836fc
Artyom Ugryumov <artiom.ygryumov@gmail.com>
2024-11-08 09:22:09 +0300

    update: Изменить интерфейс IEndpointAuth

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,6 +1,11 @@
-import { IEndpointGetMe } from './get-me'
+import { IGetRequestBody, IMeData } from '../blocks'
 
 /**
  * Ответ запроса {@link https://doc.leader.ironstar.pw/#/00-authorization/app_auth|POST /App/Auth/API?action=auth}
  */
-export interface IEndpointAuth extends IEndpointGetMe {}
+export interface IEndpointAuth extends IGetRequestBody {
+  /**
+   * Данные текущего пользователя
+   */
+  me_data: IMeData
+}
