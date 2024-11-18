1dedf7bbee754cbe58c658ce2665471cd8f7a106
Artyom Ugryumov <artiom.ygryumov@gmail.com>
2024-11-18 16:59:03 +0300

    add: Сделать диструктуризацию в каталоге blocks

--- src/blocks/index.ts
+++ src/blocks/index.ts
@@ -1,16 +1,7 @@
 export * from './me-data'
 export * from './image-object'
 export * from './kvs-property'
-
-export * from './extensions/project-access'
-export * from './extensions/get-request-body'
-export * from './extensions/table-request-body'
-
-export * from './layer/tree-layer'
-export * from './layer/polygon-view'
-export * from './layer/layer-basic'
-
-export * from './point/point-info'
-export * from './point/point-info-simple'
-
 export * from './runtime-field'
+export * from './extensions'
+export * from './layer'
+export * from './point'
