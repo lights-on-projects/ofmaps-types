a5e08f362bd7d7cc51c83b87151f8be41e30bd4b
Artyom Ugryumov <artiom.ygryumov@gmail.com>
2024-11-18 13:54:36 +0300

    add: Описать интерфейс IRunReport

---
+++ src/endpoints/run-report.ts
@@ -0,0 +1,13 @@
+import { IGetRequestBody } from '../blocks'
+
+/**
+ * Данные запроса для получения данных о конкретном отчете
+ *
+ * {@link https://doc.leader.ironstar.pw/#/05-%D0%9E%D1%82%D1%87%D0%B5%D1%82%D1%8B/meta_report_run}
+ */
+export interface IRunReport extends IGetRequestBody {
+  /**
+   * Данные отчета
+   */
+  report: Record<string, string | null>[]
+}
